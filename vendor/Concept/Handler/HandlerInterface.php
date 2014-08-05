d9a94a99deea015e0154dc949176b03eefdb9a85
Zeki ÜNAL <zekiunal@gmail.com>
2014-08-05 18:04:16 +0300

    Handler Interface added.

---
+++ vendor/Concept/Handler/HandlerInterface.php
@@ -0,0 +1,45 @@
+<?php
+namespace Concept\Handler;
+
+use Concept\Entity\EntityInterface;
+use Concept\Filter\FilterInterface;
+
+/**
+ * @author      Zeki Unal <zekiunal@gmail.com>
+ * @description
+ *
+ * @package     Concept\Query
+ * @name        QueryInterface
+ * @version     0.1
+ */
+interface HandlerInterface
+{
+    /**
+     * @param HandlerInterface $nextService
+     */
+    public function setSuccessor(HandlerInterface $nextService);
+
+    /**
+     * @param EntityInterface $entity
+     * @param                 $source
+     * @param FilterInterface $filter
+     *
+     * @return mixed
+     */
+    public function save(EntityInterface $entity, $source, FilterInterface $filter);
+
+    /**
+     * @param        FilterInterface $filter
+     *
+     * @return       array|false
+     */
+    public function load(FilterInterface $filter);
+
+    /**
+     * @param $name
+     * @param $id
+     *
+     * @return bool
+     */
+    public function delete($name, $id);
+}
