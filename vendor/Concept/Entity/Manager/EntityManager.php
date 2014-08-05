db85e28c89d5e6e45c20622d308ca1ad56278df3
Zeki Ünal <zekiunal@gmail.com>
2014-08-05 18:23:04 +0300

    Added method stubs.

--- vendor/Concept/Entity/Manager/EntityManager.php
+++ vendor/Concept/Entity/Manager/EntityManager.php
@@ -1,6 +1,9 @@
 <?php
 namespace Concept\Entity\Manager;
 
+use Concept\Filter\FilterInterface;
+use Concept\Entity\EntityInterface;
+
 /**
  * @author      Zeki Unal <zekiunal@gmail.com>
  * @description
@@ -12,4 +15,36 @@ namespace Concept\Entity\Manager;
 class EntityManager implements EntityManagerInterface
 {
 
+    /**
+     * @param EntityInterface $entity
+     * @param                 $source
+     * @param FilterInterface $filter
+     *
+     * @return array|false
+     */
+    public static function save(EntityInterface $entity, $source, FilterInterface $filter)
+    {
+        // TODO: Implement save() method.
+    }
+
+    /**
+     * @param        FilterInterface $filter
+     *
+     * @return       array|bool
+     */
+    public static function load($filter)
+    {
+        // TODO: Implement load() method.
+    }
+
+    /**
+     * @param $name
+     * @param $id
+     *
+     * @return bool
+     */
+    public static function delete($name, $id)
+    {
+        // TODO: Implement delete() method.
+    }
 }
