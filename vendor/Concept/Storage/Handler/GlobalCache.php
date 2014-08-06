8e8a520b20726393486d78f19ba8ce30ae5d907d
Zeki Ünal <zekiunal@gmail.com>
2014-08-06 17:58:36 +0300

    Global cache storage.

---
+++ vendor/Concept/Storage/Handler/GlobalCache.php
@@ -0,0 +1,105 @@
+<?php
+namespace Concept\Storage\Handler;
+
+use Concept\Entity\EntityInterface;
+use Concept\Entity\Manager\EntityManagerInterface;
+use Concept\Filter\FilterInterface;
+use Concept\Handler\HandlerInterface;
+use Concept\Cache\GlobalCache as CacheDrive;
+use Concept\Query\GlobalCache as CacheQuery;
+
+/**
+ * @author      Zeki Unal <zekiunal@gmail.com>
+ * @description
+ *
+ * @package     Concept\Storage\Handler
+ * @name        GlobalCache
+ * @version     0.1
+ */
+class GlobalCache implements HandlerInterface, EntityManagerInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected static $processor;
+
+    /**
+     * @var \Concept\Cache\GlobalCache
+     */
+    protected static $cache;
+
+    /**
+     * @param HandlerInterface $processor
+     */
+    public function setSuccessor(HandlerInterface $processor)
+    {
+        self::$processor = $processor;
+    }
+
+    public function __construct($configuration=array())
+    {
+        self::$cache = new CacheDrive($configuration);
+    }
+
+    /**
+     * @param EntityInterface $entity
+     * @param                 $source
+     * @param FilterInterface $filter
+     *
+     * @return EntityInterface
+     */
+    public static function save(EntityInterface $entity, $source, FilterInterface $filter)
+    {
+        self::$processor->save($entity, $source, $filter);
+
+        $key = CacheQuery::select($filter->setId($entity->getId()));
+
+        $entity = self::$cache->set($key, $entity);
+
+        echo "Saved by global cache handler<br>";
+
+        return $entity;
+    }
+
+    /**
+     * @param        FilterInterface $filter
+     *
+     * @return       array
+     */
+    public static function load($filter)
+    {
+
+        /**
+         * Generate key
+         */
+        $key = CacheQuery::select($filter);
+
+        $data = self::$cache->get($key);
+
+        if($data) {
+            echo "Load data from Global Cache<br>";
+            return $data;
+        }
+
+        if (!$data) {
+            $data = self::$processor->load($filter);
+            if ($data) {
+                self::$cache->set($key, $data);
+                return $data;
+            }
+        }
+        return array();
+    }
+
+
+    /**
+     * @param $name
+     * @param $id
+     *
+     * @return bool
+     */
+    public static function delete($name, $id)
+    {
+        // TODO: Implement delete() method.
+    }
+}
