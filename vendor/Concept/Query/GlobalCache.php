167f972e1d087de3b552b48a8db0faf6e649eb74
Zeki Ünal <zekiunal@gmail.com>
2014-08-06 15:56:43 +0300

    GlobalCache Query class created.

---
+++ vendor/Concept/Query/GlobalCache.php
@@ -0,0 +1,24 @@
+<?php
+namespace Concept\Query;
+use Concept\Filter\FilterInterface;
+
+/**
+ * @author      Zeki Unal <zekiunal@gmail.com>
+ * @description
+ *
+ * @package     Concept\Query
+ * @name        GlobalCache
+ * @version     0.1
+ */
+class GlobalCache
+{
+    /**
+     * @param  FilterInterface $filter
+     *
+     * @return string
+     */
+    public static function select(FilterInterface $filter)
+    {
+        return md5($filter->getSource().'_'.$filter->getId().'_'.json_encode($filter->getParameters()));
+    }
+}
