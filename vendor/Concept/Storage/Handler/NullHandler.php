293d9c8a2cb368aa83a39736b0d53895f21634fc
Zeki Ünal <zekiunal@gmail.com>
2014-08-06 17:59:15 +0300

    NullHandler storage.

---
+++ vendor/Concept/Storage/Handler/NullHandler.php
@@ -0,0 +1,54 @@
+<?php
+namespace Concept\Storage\Handler;
+
+use Concept\Filter\FilterInterface;
+use Concept\Handler\HandlerAbstract;
+use Concept\Handler\HandlerInterface;
+use Concept\Entity\EntityInterface;
+use Concept\Entity\Manager\EntityManagerInterface;
+
+/**
+ * @author      Zeki Unal <zekiunal@gmail.com>
+ * @description
+ *
+ * @package     Concept\Storage\Handler
+ * @name        NullHandler
+ * @version     0.1
+ */
+class NullHandler extends HandlerAbstract implements HandlerInterface, EntityManagerInterface
+{
+
+    /**
+     * @param EntityInterface $entity
+     * @param                 $source
+     * @param FilterInterface $filter
+     *
+     * @return EntityInterface
+     */
+    public static function save(EntityInterface $entity, $source, FilterInterface $filter)
+    {
+        return false;
+    }
+
+    /**
+     * @param        FilterInterface $filter
+     *
+     * @return       array
+     */
+    public static function load($filter)
+    {
+        echo "Load data from NullHandler<br>";
+        return array();
+    }
+
+    /**
+     * @param $name
+     * @param $id
+     *
+     * @return bool
+     */
+    public static function delete($name, $id)
+    {
+        return false;
+    }
+}
