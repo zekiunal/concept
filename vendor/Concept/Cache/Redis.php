4f50c4b6ceb09d657bb03210b90c6dbc10aeafa0
Zeki Ünal <zekiunal@gmail.com>
2014-08-07 14:38:23 +0300

    Added method stubs.

---
+++ vendor/Concept/Cache/Redis.php
@@ -0,0 +1,44 @@
+<?php
+namespace Concept\Cache;
+
+/**
+ * @author      Zeki Unal <zekiunal@gmail.com>
+ * @description
+ *
+ * @package     Concept\Cache
+ * @name        Redis
+ * @version     0.1
+ */
+class Redis implements CacheInterface
+{
+    /**
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function get($key)
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * @param string $key
+     * @param mixed  $data
+     *
+     * @return mixed
+     */
+    public function set($key, $data)
+    {
+        // TODO: Implement set() method.
+    }
+
+    /**
+     * @param string $key
+     *
+     * @return boolean
+     */
+    public function delete($key)
+    {
+        // TODO: Implement delete() method.
+    }
+}
