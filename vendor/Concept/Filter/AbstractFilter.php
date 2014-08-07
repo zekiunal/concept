ca2760a67d01cc3da3b48c26ed5786594aaa5fb3
Zeki Ünal <zekiunal@gmail.com>
2014-08-07 18:02:35 +0300

    AbstractFilter added and generated method stubs.

---
+++ vendor/Concept/Filter/AbstractFilter.php
@@ -0,0 +1,94 @@
+<?php
+namespace Concept\Filter;
+
+
+class AbstractFilter implements FilterInterface
+{
+
+    /**
+     * @param string  $column_name
+     * @param string  $value
+     * @param boolean $equal
+     *
+     * @return FilterInterface
+     */
+    public function findBy($column_name, $value, $equal = true)
+    {
+        // TODO: Implement findBy() method.
+    }
+
+    /**
+     * @return array
+     */
+    public function select()
+    {
+        // TODO: Implement select() method.
+    }
+
+    /**
+     * @return array
+     */
+    public function from()
+    {
+        // TODO: Implement from() method.
+    }
+
+    /**
+     * @return array
+     */
+    public function where()
+    {
+        // TODO: Implement where() method.
+    }
+
+    /**
+     * @return string
+     */
+    public function limit()
+    {
+        // TODO: Implement limit() method.
+    }
+
+    /**
+     * @return array
+     */
+    public function getParameters()
+    {
+        // TODO: Implement getParameters() method.
+    }
+
+    /**
+     * @return array
+     */
+    public function getSource()
+    {
+        // TODO: Implement getSource() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        // TODO: Implement getId() method.
+    }
+
+    /**
+     * @param $id
+     *
+     * @return mixed
+     */
+    public function setId($id)
+    {
+        // TODO: Implement setId() method.
+    }
+
+    /**
+     * @description
+     * @return       array
+     */
+    public function getProperties()
+    {
+        // TODO: Implement getProperties() method.
+    }
+}
