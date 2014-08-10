bf22da2c471b91d8ca2809ba507fa4ccbfbbe356
Zeki ÜNAL <zekiunal@gmail.com>
2014-08-11 01:47:20 +0300

    Example project schema sql file.

--- example/schema.sql
+++ example/schema.sql
@@ -1,18 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost_3306
-Source Server Version : 50617
-Source Host           : localhost:3306
-Source Database       : test
-
-Target Server Type    : MYSQL
-Target Server Version : 50617
-File Encoding         : 65001
-
-Date: 2014-08-11 00:37:55
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
