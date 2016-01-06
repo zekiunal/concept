665f7686240b6e69bdfccca5855115974b9691d4
Zeki ÜNAL <zekiunal@gmail.com>
2016-01-06 16:41:24 +0200

    delete added.

--- vendor/Concept/Query/MySql.php
+++ vendor/Concept/Query/MySql.php
@@ -117,4 +117,15 @@ class MySql
         $statement .= ' WHERE `' . $source . '`.`' . $source . '_id` = :' . $source . '_id';
         return $statement;
     }
+
+    /**
+     * @param string $source
+     * @return string
+     */
+    public static function delete($source)
+    {
+        $statement  = "DELETE FROM `" . $source . "`";
+        $statement .= ' WHERE `' . $source . '`.`' . $source . '_id` = :' . $source . '_id';
+        return $statement;
+    }
 }
